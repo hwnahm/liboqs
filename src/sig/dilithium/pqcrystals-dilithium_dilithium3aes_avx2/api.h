89ea51ea3c8fff5e0f45fda3b31738153e72e322
Peter H. Nahm <hwnahm@gmail.com>
2022-10-28 22:47:23 +0900

    using seed for key pair

--- src/sig/dilithium/pqcrystals-dilithium_dilithium3aes_avx2/api.h
+++ src/sig/dilithium/pqcrystals-dilithium_dilithium3aes_avx2/api.h
@@ -60,7 +60,7 @@ int pqcrystals_dilithium2aes_avx2_open(uint8_t *m, size_t *mlen,
 #define pqcrystals_dilithium3_avx2_SECRETKEYBYTES pqcrystals_dilithium3_SECRETKEYBYTES
 #define pqcrystals_dilithium3_avx2_BYTES pqcrystals_dilithium3_BYTES
 
-int pqcrystals_dilithium3_avx2_keypair(uint8_t *pk, uint8_t *sk);
+int pqcrystals_dilithium3_avx2_keypair(uint8_t *pk, uint8_t *sk, uint8_t *seed);
 
 int pqcrystals_dilithium3_avx2_signature(uint8_t *sig, size_t *siglen,
                                         const uint8_t *m, size_t mlen,
